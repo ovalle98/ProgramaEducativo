a1260f83f7c087681a4c715edb69ec7efbdf76a1
ovalle98 <ovallejuan98@gmail.com>
2026-06-27 21:10:37 -0600

    Update structure table inscripciones

--- backend/src/ddl/ddl_programa_educativo.sql
+++ backend/src/ddl/ddl_programa_educativo.sql
@@ -75,8 +75,7 @@ CREATE TABLE IF NOT EXISTS inscripciones (
 	CONSTRAINT FK_INSCRIPCIONES_IDPROGRAMA FOREIGN KEY (id_programa)
 		REFERENCES programas (id_programa) ON UPDATE RESTRICT ON DELETE RESTRICT,
 	CONSTRAINT FK_INSCRIPCIONES_IDESTATUS FOREIGN KEY (id_estatus)
-		REFERENCES estatus (id_estatus) ON UPDATE RESTRICT ON DELETE RESTRICT,
-	CONSTRAINT CHK_INSCRIPCIONES_ESTATUS CHECK (id_estatus IN (1, 2, 3))
+		REFERENCES estatus (id_estatus) ON UPDATE RESTRICT ON DELETE RESTRICT
 );
 
 -- =============================================================================
